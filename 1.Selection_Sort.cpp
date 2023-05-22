a337effebbc0cee98ac1a10268854ca27da46e59
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-23 03:19:02 +0530

    Selection sort here

--- 1.Selection_Sort.cpp
+++ 1.Selection_Sort.cpp
@@ -2,5 +2,21 @@
 #include<vector>
 using namespace std;
 int main(){
+    vector<int>v{5,4,3,2,1};
+    int n = v.size();
 
+    //selection sort
+    for(int i = 0; i< n-1; i++){
+        int minIndex =i;
+        for(int j = i+1; j<n;j++){
+            if(v[j]<v[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(v[i],v[minIndex]);
+    }
+    for(int i =0; i < n; i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
 }
