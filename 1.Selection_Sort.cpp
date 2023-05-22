56366c6aa3a0b3ee363a47cde555dd995e189061
Saurabh Gupta <saurabhgupta9369899665@gmail.com>
2023-05-23 04:33:23 +0530

    bubble sort done

--- 1.Selection_Sort.cpp
+++ 1.Selection_Sort.cpp
@@ -1,22 +1,27 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    vector<int>v{5,4,3,2,1};
+int main()
+{
+    vector<int> v{5, 4, 3, 2, 1};
     int n = v.size();
 
-    //selection sort
-    for(int i = 0; i< n-1; i++){
-        int minIndex =i;
-        for(int j = i+1; j<n;j++){
-            if(v[j]<v[minIndex]){
+    // selection sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[j] < v[minIndex])
+            {
                 minIndex = j;
             }
         }
-        swap(v[i],v[minIndex]);
+        swap(v[i], v[minIndex]);
     }
-    for(int i =0; i < n; i++){
-        cout<<v[i]<<" ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
--- 2.Bubble_Sort.cpp
+++ 2.Bubble_Sort.cpp
@@ -1,6 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    
+int main()
+{
+    vector<int> v{1, 7, 6, 10, 9, 14};
+    int n = v.size();
+    // bubble sort
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                swap(v[j], v[j + 1]);
+            }
+        }
+    }
+    // printing
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
